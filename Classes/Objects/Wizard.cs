c51e283cc7cae2afee3efd4e443d3a31dd478d3c
davi3553 <dada5@student.eadania.dk>
2025-02-25 10:56:47 +0100

    Classes
    Classes er blevet tilføjet

---
+++ Classes/Objects/Wizard.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DAlgorithms.Classes.Objects
+{
+    class Wizard
+    {
+    }
+}
